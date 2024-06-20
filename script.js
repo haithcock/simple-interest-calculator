7257bca6950760bfcba096f4803d553008c12d58
haithcock <166430811+haithcock@users.noreply.github.com>
2024-06-20 13:28:58 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+document.getElementById("rate").addEventListener("input", updateRate);
+
 function compute() {
     var principal = document.getElementById("principal").value;
     var rate = document.getElementById("rate").value;
@@ -10,14 +12,12 @@ function compute() {
     if (principal <= 0) {
         alert('Please enter a positive number!');
         document.getElementById("principal").focus();
-    }
-    else {
+    } else {
         result.innerHTML = "If you deposit $" + "<mark>" + principal + "</mark>" + ",\<br\> at an interest rate of " + "<mark>" + rate + "%" + "</mark>" + "\<br\> You will receive an amount of $" + "<mark>" + amount + "</mark>" + ",\<br\> in the year " + "<mark>" + year + "</mark>" + "\<br\>";
     } 
 }
 
-function updateRate()
-{
+function updateRate() {
     var rateval = document.getElementById("rate").value;
     document.getElementById("rate_val").innerText = rateval;
 }
